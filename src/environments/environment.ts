b3768765c4d3439e4c28cb88948eb15d4e3aeeaf
devJayesh14 <167863035+devJayesh14@users.noreply.github.com>
2025-12-24 10:16:32 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://cricket-auction-backend-sable.vercel.app'
+  apiUrl: 'https://cricket-auction-backend-oktktec79-devjayesh03s-projects.vercel.app'
 };
+
 
